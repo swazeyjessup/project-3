37ab4b01e86f6ce87f584adf3586fc1a2001a073
Swazey Jessup <swazey_jessup@hotmail.com>
2020-05-24 13:13:39 -0600

    added to leaderboard and habits

---
+++ src/pages/Habits.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Habits extends Component {
+
+    render() {
+        return(
+            <div>
+                <h1>Habits</h1>
+            </div>
+        )
+    }
+
+
+}
+
+export default Habits;
---
+++ src/pages/Leaderboard.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Leaderboard extends Component {
+
+    render() {
+        return(
+            <div>
+                <h1>Leaderboard</h1>
+            </div>
+        )
+    }
+
+
+}
+
+export default Leaderboard;
