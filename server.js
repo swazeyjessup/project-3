fb1cfa1f3e9a2ba2097ae9fc596fbecdaefb0133
Jean Peel <jeanine.peel@gmail.com>
2020-05-26 17:03:05 -0600

    minor changes

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const PORT = process.env.PORT || 3030;
 const app = express();
 const mongoose = require('mongoose');
 
-// mongoose.Promise = global.Promise;
-// mongoose.connect("mongodb://localhost:27017/HabitTracker", { 
-//     useNewUrlParser: true 
-// });
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/HabitTracker", { 
+    useNewUrlParser: true 
+});
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -18,40 +18,40 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define any API routes before this runs
-// app.get("*", (request, response) => {
-//     ToDoItem.find({})
-//     .then(function (data) {
-//     response.status(200).json(data);
-//     })
-//     .catch(function () {
-//     response.status(404).end("404!! Information BLACK HOLE!!");
-//     });
-// });
+app.get("*", (request, response) => {
+    ToDoItem.find({})
+    .then(function (data) {
+    response.status(200).json(data);
+    })
+    .catch(function () {
+    response.status(404).end("404!! Information BLACK HOLE!!");
+    });
+});
 
-// app.delete("*", (request, response) => {
-// const mongoID = request.params.id;
-// ToDoItem.remove({
-//     _id: mongoID,
-// })
-// .then((data) => {
-//     response.status(200).end();
-// })
-// .catch((error) => {
-//     response.status(404).send(error.message);
-// });
-// });
+app.delete("*", (request, response) => {
+const mongoID = request.params.id;
+ToDoItem.remove({
+    _id: mongoID,
+})
+.then((data) => {
+    response.status(200).end();
+})
+.catch((error) => {
+    response.status(404).send(error.message);
+});
+});
 
-// app.post("*", (request, response) => {
-// const bookData = request.body;
-// console.log(bookData)
-// ToDoItem.create(bookData)
-// .then(function () {
-//     response.status(200).end();
-// })
-// .catch(function (error) {
-//     response.status(404).send(error.message);
-// });
-// });
+app.post("*", (request, response) => {
+const bookData = request.body;
+console.log(bookData)
+ToDoItem.create(bookData)
+.then(function () {
+    response.status(200).end();
+})
+.catch(function (error) {
+    response.status(404).send(error.message);
+});
+});
 
 app.listen(PORT, () => {
 console.log(`🌎 ==> API server now on port ${PORT}!`);
--- src/pages/Habits.js
+++ src/pages/Habits.js
@@ -1,16 +1,69 @@
+
 import React, { Component } from "react";
+import AddToDo from "../components/AddToDo";
+import ToDoList from "../components/ToDoList";
 
-class Habits extends Component {
+class Form extends Component {
+state = {
+    toDoItem: '',
+    toDoList: [
+    {
+        text: 'Display to do item',
+        done: true
+    }
+    ]
+};
 
-    render() {
-        return(
-            <div>
-                <h1>Habits</h1>
-            </div>
-        )
+updateToDoItem = (event) => {
+    const { name, value } = event.target;
+
+    this.setState({
+    [name]: value
+    })
+}
+
+saveToDoItem = (event) => {
+    event.preventDefault();
+
+    const newToDoItem = {
+    text: this.state.toDoItem,
+    done: false
     }
 
+    this.setState({
+    toDoList: this.state.toDoList.concat(newToDoItem),
+    toDoItem: ''
+    })
+}
+
+markAsDone = (event) => {
+    const index = event.target.value;
+    let toDoItem = this.state.toDoList[index];
+    toDoItem.done = !toDoItem.done;
+
+    this.setState({
+    toDoList: this.state.toDoList
+    })
+}
+
+
+render() {
+return (
+    <div>
+    <h1>To Do List</h1>
+    <AddToDo
+        toDoItem={this.state.toDoItem}
+        updateToDoItem={this.updateToDoItem}
+        saveToDoItem={this.saveToDoItem}
+    />
+    <ToDoList
+        toDoList={this.state.toDoList}
+        markAsDone={this.markAsDone}
+    />
+    </div> 
+);
+}
 
 }
 
-export default Habits;
+export default Form;
