3e6d63ad70d1b13f531bceaaff94c9133354de9c
Nilsen <58581719+nreyes1202@users.noreply.github.com>
2020-05-27 13:09:07 -0600

    restructured files

---
+++ client/src/pages/Login.js
@@ -0,0 +1,3 @@
+//onchange handler to save what's in input fields (saves to state)
+//onclick function
+//axios call 
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 const PORT = process.env.PORT || 3030;
 const app = express();
 const mongoose = require('mongoose');
-const Habit = require('src/components/SaveToDo/');
+//const Habit = require('src/components/SaveToDo/');
 
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost:27017/HabitTracker", { 
