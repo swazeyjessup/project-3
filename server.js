2cf886c7d2990c424edaf28f85f9dfd7849011a9
Nilsen <58581719+nreyes1202@users.noreply.github.com>
2020-05-27 20:05:37 -0600

    added code to server file

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
-var express = require("express");
+const express = require("express");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 const PORT = process.env.PORT || 3030;
 const app = express();
 const mongoose = require('mongoose');
-//const Habit = require('src/components/SaveToDo/');
+const passport = require("./passport/setup");
+const auth = require("./routes/auth");
+//add route to login page - ask Chris?
+const MONGO_URI = "mongodb://localhost:27017";
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/HabitTracker", { 
-    useNewUrlParser: true 
-});
+mongoose.connect(MONGO_URI, "mongodb://localhost:27017/HabitTracker", { 
+    useNewUrlParser: true })
+    .then(console.log(`MongoDB connected ${MONGO_URI}`))
+    .catch(err => console.log(err));
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -17,6 +23,24 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
+
+// Express Session
+app.use(
+    session({
+        secret: "Secret password!",
+        resave: false,
+        saveUninitialized: true,
+        store: new MongoStore({ mongooseConnection: mongoose.connection })
+    })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Routes
+app.use("/api/auth", auth);
+app.get("/", (req, res) => res.send("Hello!"));
 
 // Define any API routes before this runs
 app.get("*", (request, response) => {
