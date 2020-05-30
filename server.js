1d34c0a8ff509388ce535ccd7610d6fc69f06de9
Nilsen <58581719+nreyes1202@users.noreply.github.com>
2020-05-30 13:07:56 -0600

    commented out database code for login

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
 const express = require("express");
-const session = require("express-session");
-const MongoStore = require("connect-mongo")(session);
 const PORT = process.env.PORT || 3030;
 const app = express();
 const mongoose = require('mongoose');
-const passport = require("./client/src/passport/setup");
-const auth = require("./client/src/routes/auth");
-//add route to login page - ask Chris?
-const MONGO_URI = "mongodb://localhost:27017/login";
 const Habit = require('./client/src/models/Habit');
+// const passport = require("./client/src/passport/setup");
+// const auth = require("./client/src/routes/auth");
+// const session = require("express-session");
+// const MongoStore = require("connect-mongo")(session);
+
 
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGO_URI, "mongodb://localhost:27017/HabitTracker", {
+mongoose.connect("mongodb://localhost:27017/HabitTracker", {
     useNewUrlParser: true
 })
-    .then(console.log(`MongoDB connected ${MONGO_URI}`))
+    .then(console.log(`MongoDB connected`))
     .catch(err => console.log(err));
 
 
@@ -29,27 +28,27 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-}
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static("client/build"));
+// }
 
-// Express Session
-app.use(
-    session({
-        secret: "Secret password!",
-        resave: false,
-        saveUninitialized: true,
-        store: new MongoStore({ mongooseConnection: mongoose.connection })
-    })
-);
+// // Express Session
+// app.use(
+//     session({
+//         secret: "Secret password!",
+//         resave: false,
+//         saveUninitialized: true,
+//         store: new MongoStore({ mongooseConnection: mongoose.connection })
+//     })
+// );
 
-// Passport middleware
-app.use(passport.initialize());
-app.use(passport.session());
+// // Passport middleware
+// app.use(passport.initialize());
+// app.use(passport.session());
 
-// Routes
-app.use("/api/auth", auth);
-app.get("/", (req, res) => res.send("Hello!"));
+// // Routes
+// app.use("/api/auth", auth);
+// app.get("/", (req, res) => res.send("Hello!"));
 
 // Define any API routes before this runs
 app.get("*", (request, response) => {
